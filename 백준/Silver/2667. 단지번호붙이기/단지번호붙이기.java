f9477200df1db865bdc962d011bd92ee42ff1bcf
Jaeukhan <77158873+Jaeukhan@users.noreply.github.com>
2022-08-20 20:03:54 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,16 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 	static char map[][];
 	static boolean visited[][];
 	static LinkedList<Integer> homes;
-	static int di[] = {-1,1,0,0};
-	static int dj[] = {0,0,-1,1};
+	static int di[] = { -1, 1, 0, 0 };
+	static int dj[] = { 0, 0, -1, 1 };
 	static int n;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -24,12 +26,12 @@ public class Main {
 		}
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if(map[i][j]=='1'&&!visited[i][j]) {
-					int cnt = dfs(i,j,1);
-					if(cnt !=0 ) {
+				if (map[i][j] == '1' && !visited[i][j]) {
+					int cnt = bfs(i, j, 0);
+					if (cnt != 0) {
 						homes.add(cnt);
 					}
-					
+
 				}
 			}
 		}
@@ -38,17 +40,36 @@ public class Main {
 		for (int i = 0; i < homes.size(); i++) {
 			System.out.println(homes.get(i));
 		}
-		
+
 	}
 
-	private static int dfs(int i, int j, int cnt) {
+	static class Cur {
+		int i, j;
+
+		public Cur(int x, int y) {
+			this.i = x;
+			this.j = y;
+		}
+
+	}
+
+	private static int bfs(int i, int j, int cnt) {
+		Queue<Cur> que = new ArrayDeque<>();
 		visited[i][j] = true;
-		for(int d=0;d<di.length;d++) {
-			int ni = i+di[d];
-			int nj= j+dj[d];
-			if(0<=ni&&ni<n&&0<=nj&&nj<n&&!visited[ni][nj]&&map[ni][nj]=='1') {
-				cnt = dfs(ni, nj, cnt+1);
+		que.offer(new Cur(i, j));
+		cnt++;
+		while (!que.isEmpty()) {
+			Cur current = que.poll();
+			for (int d = 0; d < di.length; d++) {
+				int ni = current.i + di[d];
+				int nj = current.j + dj[d];
+				if (0 <= ni && ni < n && 0 <= nj && nj < n && !visited[ni][nj] && map[ni][nj] != '0') {
+					que.offer(new Cur(ni, nj));
+					visited[ni][nj] = true;
+					cnt++;
+				}
 			}
+
 		}
 		return cnt;
 	}
