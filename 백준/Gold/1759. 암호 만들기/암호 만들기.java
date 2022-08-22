28b0337ba62aa0105a19b0a4152420129b598f56
Jaeukhan <77158873+Jaeukhan@users.noreply.github.com>
2022-08-22 12:59:52 +0900

    [Gold V] Title: 암호 만들기, Time: 180 ms, Memory: 16484 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -2,45 +2,54 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int l, c;
-	static char[] arr, results;
+	static char[] txt;
+	static boolean[] select;
 	static String mo = "aeiou";
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		l = Integer.parseInt(st.nextToken()); c = Integer.parseInt(st.nextToken());
-		arr = new char[c];
-		results = new char[l];
+		l = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		select = new boolean[c];
+		txt = new char[c];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = st.nextToken().toCharArray()[0];
-		}//input end
-		Arrays.sort(arr);
-		perm(0, 0,-1);
+		for (int i = 0; i < c; i++) {
+			txt[i] = st.nextToken().toCharArray()[0];
+		}
+		Arrays.sort(txt);
+		comb(0, 0);
 	}
-	private static void perm(int idx, int flag, int before) {
-		if(idx==l) {
-			String tmp="";
-			int mocnt=0, jacnt=0;
-			for (int i = 0; i < results.length; i++) {
-				if(mo.contains(Character.toString(results[i]))) {
-					mocnt ++;
-				}else jacnt++;
-				tmp+=results[i];
+
+	private static void comb(int idx, int cnt) {
+		if (cnt == l) {
+			String tmp = "";
+			int moc = 0, jac = 0;
+			for (int i = 0; i < c; i++) {
+				if (select[i]) {
+					if (mo.contains(Character.toString(txt[i]))) {
+						moc++;
+					} else {
+						jac++;
+					}
+					tmp += txt[i];
+				}
 			}
-			if(mocnt>=1 && jacnt>=2) {
+			if (moc >= 1 && jac >= 2) {
 				System.out.println(tmp);
 			}
 			return;
 		}
-		for(int i=0;i<arr.length;i++) {
-			if((flag&1<<i)!=0||before>arr[i]-'a') continue;
-			results[idx] = arr[i];
-			perm(idx+1, flag|1<<i, arr[i]-'a');
-		}
+		if (idx == c)
+			return;
+		select[idx] = true;
+		comb(idx + 1, cnt + 1);
+		select[idx] = false;
+		comb(idx + 1, cnt);
 	}
+
 }
