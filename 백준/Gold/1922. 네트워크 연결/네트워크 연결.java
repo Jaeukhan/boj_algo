8c790347fd2315e50c3be32b1738ea9e8a224b91
Jaeukhan <77158873+Jaeukhan@users.noreply.github.com>
2022-08-25 13:29:12 +0900

    [Gold IV] Title: 네트워크 연결, Time: 540 ms, Memory: 53176 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -3,59 +3,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int n, m;
-	static LinkedList<Edge> adjList;
-	static int[] parents;
+	static LinkedList<Edge> adjList[];
+	static boolean[] visited;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		n = Integer.parseInt(br.readLine());
 		m = Integer.parseInt(br.readLine());
-		parents = new int[n+1];
-		adjList = new LinkedList<>();
+		adjList = new LinkedList[n+1];
+		for(int i=1;i<=n;i++) {
+			adjList[i] = new LinkedList<>();
+		}
+		visited = new boolean[n+1];
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken()), to = Integer.parseInt(st.nextToken()),
 					weight = Integer.parseInt(st.nextToken());
-			adjList.add(new Edge(from, to, weight));
-		}
-		Collections.sort(adjList);
-		make();
+			adjList[from].add(new Edge(to, weight));
+			adjList[to].add(new Edge(from, weight));
+		}//input end
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.add(new Edge(1, 0));
 		long result = 0, cnt = 0;
-		for(Edge edge: adjList) {
-			if(union(edge.from, edge.to)) {
-				result += edge.weight;
-				if(++cnt==n-1) break;
+		while(!pq.isEmpty()) {
+			Edge minedge = pq.poll();
+			if(visited[minedge.num]) continue;
+			result += minedge.weight;
+			visited[minedge.num] = true;
+			if(cnt++==n) break;
+			for(Edge edge: adjList[minedge.num]) {
+				if(!visited[edge.num]) {
+					pq.add(edge);
+				}
 			}
 		}
 		System.out.println(result);
 	}
-	static void make() {
-		for(int i=1;i<=n;i++) {
-			parents[i] = i;
-		}
-	}
-	static int find(int a) {
-		if(a==parents[a]) return a;
-		return parents[a] = find(parents[a]);
-	}
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if(aRoot==bRoot) return false;
-		parents[bRoot] = aRoot;
-		return true;
-	}
-	static class Edge implements Comparable<Edge> {
-		int from, to, weight;
 
-		public Edge(int from, int to, int weight) {
-			this.from = from;
-			this.to = to;
+	static class Edge implements Comparable<Edge> {
+		int num, weight;
+
+		public Edge(int num, int weight) {
+			this.num = num;
 			this.weight = weight;
 		}
 
