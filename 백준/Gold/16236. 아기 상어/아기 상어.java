79f7365e61e4e067daa67b8b5fca2d84d1a92e7c
Jaeukhan <77158873+Jaeukhan@users.noreply.github.com>
2022-08-26 10:41:43 +0900

    [Gold III] Title: 아기 상어, Time: 248 ms, Memory: 18548 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,124 +1,111 @@
-import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	static int n, fishes, ans;
-	static int map[][];
-	static boolean visited[][];
-	static Queue<Cur> que;
-	static int di[] = {-1,1,0,0};
-	static int dj[] = {0,0,-1,1};
+	static int sharki, sharkj, sharkEat, sharkSize;
+	static int N;
+	static int[][] map;
+	static boolean[][] visit; // 먹이 탐색하면서 큐에 중복칸 들어가지 말라고 쓸 배열
+	static int ans;
+	
+	static int[] di = {0,0,1,-1};
+	static int[] dj = {1,-1,0,0};
 	
 	public static void main(String[] args) {
-//		float st = System.nanoTime();
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		map = new int[n][n];
-		fishes = 0;ans = 0;
-		Cur start = new Cur(0,0,2,2);;
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
+
+		N = sc.nextInt();
+		map = new int[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
 				map[i][j] = sc.nextInt();
-				if(map[i][j] == 9) {
-					start = new Cur(i,j,2,2);
-					map[i][j] = 0;
-				}else if(0<map[i][j]&&map[i][j]<=6) {
-					fishes++;
+				if (map[i][j] == 9) { // 초기 상어 위치
+					sharki = i;
+					sharkj = j;
+					sharkSize = 2; // 초기 크기 2
+					sharkEat = 0;
+					map[i][j] = 0; // 지도에 있는 상어 공중부양.
 				}
 			}
-		}//input end
-		if(fishes==0) {
-			System.out.println(0);
-		}else {
-			bfs(start);
-			System.out.println(ans);
-		}
-//			float en = System.nanoTime();
-//		System.out.println((en-st)+" s");
+		} // end input
 		
+		while(true) {
+			if(!findFeed()) break;
+		}
+		System.out.println(ans);
 	}
-	private static void bfs(Cur start) {
-		que = new ArrayDeque<>();
-		que.add(start);
-		visited = new boolean[n][n];
-		visited[start.i][start.j] = true;
-		PriorityQueue<Cur> eating = new PriorityQueue<>();
-		int time = 0;
-		while(!que.isEmpty()) {
-			int size = que.size();
-			while(--size>=0) {
-				Cur cur = que.poll();
-				if(map[cur.i][cur.j]!=0&&map[cur.i][cur.j]<cur.size) {
-					eating.add(new Cur(cur.i, cur.j, time, time));
-				}
-				for(int d=0;d<4;d++) {
-					int ni = cur.i+di[d];
-					int nj = cur.j+dj[d];
-					if(0<=ni&&ni<n&&0<=nj&&nj<n&&map[ni][nj]<=cur.size&&!visited[ni][nj]) {
-						que.add(new Cur(ni,nj, cur.size,cur.need));
-						visited[ni][nj] = true;
+	
+	private static boolean findFeed() { // 상어 위치에서 먹이탐색 시작. 가까운데 있음 좋겠네
+		Queue<Point> queue = new LinkedList<>();
+		visit = new boolean[N][N];
+		
+		queue.add(new Point(sharki, sharkj));
+		visit[sharki][sharkj] = true;
+		
+		int dist=0;
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			PriorityQueue<Point> feed = new PriorityQueue<>();
+			for(int s=0; s<size; s++) {
+				Point now = queue.poll();
+				for(int d=0; d<4; d++) {
+					int nexti = now.i + di[d];
+					int nextj = now.j + dj[d];
+					if(nexti>=0 && nexti<N && nextj>=0 && nextj<N) { // 배열 유효범위
+						if(map[nexti][nextj]>0 && map[nexti][nextj]<sharkSize) { // 먹이다!
+							feed.add(new Point(nexti, nextj));
+						}
+						if((map[nexti][nextj]==0||map[nexti][nextj]==sharkSize) && !visit[nexti][nextj]) {
+							queue.add(new Point(nexti, nextj));
+							visit[nexti][nextj]=true;
+						}
 					}
 				}
 			}
-			time++;
-		}
-//		print();
-		if(eating.size()==0) return;
-		Cur small = eating.poll();
-		ans+= small.need;
-		if(--fishes==0) return;
-		int i=small.i,j=small.j,size = start.size,need = start.need-1;
-		map[i][j] = 0;
-//		System.out.println("현재까지 시간 "+ans+" 추가되는 시간 "+time+" 현재크기 "+size+" 필요한 물고기 "+need );
-		if(need==0) {
-			bfs(new Cur(i,j,size+1,size+1));						
-		}else {
-			bfs(new Cur(i,j,size,need));			
-		}
-	}
-	static void print() {
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-				System.out.print(map[i][j]);
+			if(!feed.isEmpty()) {
+				Point f = feed.poll();
+				sharkEat++;
+				map[f.i][f.j] = 0; // 해당 먹이 먹고
+				sharki = f.i; // 먹이 위치로 이동
+				sharkj = f.j;
+				if(sharkEat == sharkSize) { // 상어 성장
+					sharkSize++;
+					sharkEat=0;
+				}
+				ans += dist+1; // 이번 먹이를 먹기위해 상어가 움직인 거리
+				return true;
 			}
-			System.out.println();
-		}
-		System.out.println();
+			dist++;
+		} // end while
+		return false;
 	}
-	static class Cur implements Comparable<Cur>{
-		int i, j, size=0, need=0;
-		public Cur(int i, int j) {
+
+	static class Point implements Comparable<Point>{
+		int i, j;
+		Point(int i, int j){
 			this.i = i;
 			this.j = j;
-		}
-		public Cur(int i, int j, int size,int need) {
-			super();
-			this.i = i;
-			this.j = j;
-			this.size = size;
-			this.need = need;
 		}
 		@Override
-		public int compareTo(Cur o) {
-			if(this.size>o.size)
-				return +1;
-			else if(this.size==o.size) {
-				if(this.i>o.i) 
-					return +1;
-				else if(this.i==o.i) {
-					if(this.j>o.j)
-						return +1;
-					else
-						return-1;
-				}else {
-					return -1;
-				}
-			}
+		public int compareTo(Point o) {
+//			if(this.i == o.i)
+//				return this.j - o.j;
+//			return this.i - o.i;
 			
-			return -1;
+			if(this.i > o.i)
+				return +1;
+			else if(this.i < o.i)
+				return -1;
+			else { // i가 똑같아?
+				if(this.j > o.j)
+					return +1;
+				else if(this.j < o.j)
+					return -1;
+			}
+			return 0;
 		}
-		
 	}
 }
